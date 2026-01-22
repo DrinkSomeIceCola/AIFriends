63aab1359a664035ed9a7030a35b6feed731e1fc
ice <837137678@qq.com>
2026-01-22 21:24:55 +0800

    初始化

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,14 +10,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
   build: {
     outDir: path.resolve(__dirname, '../backend/static/frontend'), // 打包到 Django static
     emptyOutDir: true,
   },
   plugins: [
+    tailwindcss(),
     vue(),
     vueDevTools(),
   ],
