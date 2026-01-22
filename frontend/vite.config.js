2d4be17c2500aa36fed6a799b2178985af078b1c
ice <837137678@qq.com>
2026-01-22 21:10:00 +0800

    初始化项目

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   build: {
     outDir: path.resolve(__dirname, '../backend/static/frontend'), // 打包到 Django static
     emptyOutDir: true,
