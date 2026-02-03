fc56cd4a63c4af924d55329055dbe31b4b66c200
ice <837137678@qq.com>
2026-02-03 21:12:45 +0800

    重命名组件

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,64 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Homepage from "@/views/homepage/Homepage.vue";
-import FriendIcon from "@/components/navbar/icons/FriendIcon.vue";
-import Friend from "@/views/friend/Friend.vue";
-import Create from "@/views/create/Create.vue";
-import NotFound from "@/views/error/NotFound.vue";
-import Login from "@/views/user/account/Login.vue";
-import Register from "@/views/user/account/Register.vue";
-import Space from "@/views/user/space/Space.vue";
-import Profile from "@/views/profile/Profile.vue";
+import HomepageIndex from "@/views/homepage/HomepageIndex.vue";
+import FriendIndex from "@/views/friend/FriendIndex.vue";
+import CreateIndex from "@/views/create/CreateIndex.vue";
+import NotFoundIndex from "@/views/error/NotFoundIndex.vue";
+import LoginIndex from "@/views/user/account/LoginIndex.vue";
+import RegisterIndex from "@/views/user/account/RegisterIndex.vue";
+import SpaceIndex from "@/views/user/space/SpaceIndex.vue";
+import ProfileIndex from "@/views/user/profile/ProfileIndex.vue";
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-      {
-          path:'/',
-          component: Homepage,
-          name: 'homepage',
-      },
-      {
-          path:'/friend',
-          component: Friend,
-          name: 'friend',
-      },
-      {
-          path:'/create',
-          component: Create,
-          name: 'create',
-      },
-      {
-          path:'/404',
-          component: NotFound,
+        {
+          path: '/',
+          component: HomepageIndex,
+          name: 'homepage-index',
+        },
+        {
+          path: '/friend/',
+          component: FriendIndex,
+          name: 'friend-index',
+        },
+        {
+          path: '/create/',
+          component: CreateIndex,
+          name: 'create-index',
+        },
+        {
+          path: '/404/',
+          component: NotFoundIndex,
           name: '404',
-      },
-      {
-          path:'/user/account/login',
-          component: Login,
-          name: 'login',
-      },
-      {
-          path:'/user/account/register',
-          component: Register,
-          name: 'register',
-      },
-      {
-          path:'/user/space/:user_id',
-          component: Space,
-          name: 'space',
-      },
-      {
-          path:'/user/profile',
-          component: Profile,
-          name: 'profile',
-      },
-      {
-          path:'/:pathMatch(.*)*',
-          component: NotFound,
-          name: 'NotFound',
-      },
-  ],
+        },
+        {
+          path: '/user/account/login/',
+          component: LoginIndex,
+          name: 'user-account-login-index',
+        },
+        {
+          path: '/user/account/register/',
+          component: RegisterIndex,
+          name: 'user-account-register-index',
+        },
+        {
+          path: '/user/space/:user_id/',
+          component: SpaceIndex,
+          name: 'user-space-index',
+        },
+        {
+          path: '/user/profile/',
+          component: ProfileIndex,
+          name: 'user-profile-index',
+        },
+        {
+          path: '/:pathMatch(.*)*',
+          component: NotFoundIndex,
+          name: 'not-found',
+        },
+],
 })
 
 export default router
