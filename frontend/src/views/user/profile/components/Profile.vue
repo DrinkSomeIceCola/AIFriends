8eb189edc2a5f5ce312a43c1198c4b143177b77b
ice <837137678@qq.com>
2026-02-14 23:04:00 +0800

    编辑资料功能-进行中

--- frontend/src/views/user/profile/components/Profile.vue
+++ frontend/src/views/user/profile/components/Profile.vue
@@ -1,5 +1,12 @@
 <script setup>
+import {ref, watch} from "vue";
 
+const props = defineProps(['profile'])
+const myUsername = ref(props.profile)
+
+watch(() => props.username, newVal => {
+  myUsername.value = newVal
+})
 </script>
 
 <template>
--- frontend/src/views/user/profile/components/Username.vue
+++ frontend/src/views/user/profile/components/Username.vue
@@ -1,5 +1,12 @@
 <script setup>
+import {ref, watch} from "vue";
 
+const props = defineProps(['username'])
+const myUsername = ref(props.username)
+
+watch(() => props.username, newVal => {
+  myUsername.value = newVal
+})
 </script>
 
 <template>
