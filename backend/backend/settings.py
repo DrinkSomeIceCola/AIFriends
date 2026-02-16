7d8fa500c0b658a1dc4a37dfddfb344df82a7f8f
ice <837137678@qq.com>
2026-02-16 15:42:20 +0800

    实现资料编辑页面前端

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -157,5 +157,5 @@ SIMPLE_JWT = {
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
--- frontend/src/views/user/profile/components/Username.vue
+++ frontend/src/views/user/profile/components/Username.vue
@@ -7,10 +7,17 @@ const myUsername = ref(props.username)
 watch(() => props.username, newVal => {
   myUsername.value = newVal
 })
+
+defineExpose({
+  myUsername,
+})
 </script>
 
 <template>
-
+  <fieldset class="fieldset">
+    <label class="label text-base">用户名</label>
+    <input v-model="myUsername" type="text" class="input w-108">
+  </fieldset>
 </template>
 
 <style scoped>
