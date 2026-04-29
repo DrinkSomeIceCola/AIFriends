137698cb5a8cb06769704cd3ea197139e01ced59
ice <837137678@qq.com>
2026-04-29 09:47:15 +0800

    实现流式返回结果

--- frontend/src/components/character/chat_field/input_field/InputField.vue
+++ frontend/src/components/character/chat_field/input_field/InputField.vue
@@ -1,22 +1,64 @@
 <script setup>
 import SendIcon from "@/components/character/icons/SendIcon.vue";
 import MicIcon from "@/components/character/icons/MicIcon.vue";
+import {useTemplateRef,ref} from "vue";
+import streamApi from "@/js/http/streamApi.js";
+
+const props = defineProps(['friendId'])
+const inputRef = useTemplateRef('input-ref')
+const message = ref('')
+let isProcessing = false
+
+function focus() {
+  inputRef.value.focus()
+}
+
+defineExpose({
+  focus,
+})
+
+async function handleSend(){
+  const content = message.value.trim()
+  if (!content) return
+  message.value = ''
+  try {
+    const res = await streamApi('/api/friend/message/chat/',{
+      body: {
+        friend_id: props.friendId,
+        message: content,
+      },
+      onmessage(data,isDone) {
+        if (isDone) {
+          isProcessing = false
+        }
+        console.log(data)
+      },
+      onerror(err) {
+        isProcessing = false
+      },
+    })
+  }catch (err){
+    isProcessing = false
+  }
+}
 </script>
 
 <template>
-  <div class="absolute bottom-4 left-2 h-12 w-86 flex items-center">
+  <form @submit.prevent="handleSend" class="absolute bottom-4 left-2 h-12 w-86 flex items-center">
     <input
+        ref="input-ref"
+        v-model="message"
         class="input bg-black/30 backdrop-blur-sm text-white text-base w-full h-full rounded-2xl pr-20"
         type="text"
         placeholder="文本输入..."
     >
-    <div class="absolute right-2 w-8 h-8 flex justify-center items-center cursor-pointer">
+    <div @click="handleSend" class="absolute right-2 w-8 h-8 flex justify-center items-center cursor-pointer">
       <SendIcon />
     </div>
     <div class="absolute right-10 w-8 h-8 flex justify-center items-center cursor-pointer">
       <MicIcon />
     </div>
-  </div>
+  </form>
 </template>
 
 <style scoped>
